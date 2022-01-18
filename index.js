5158dbe8bec7cfb2da5754ce992171f24593c201
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-01-18 09:49:06 -0800

    added most images issue on line 17 event listenener is not a function? Need to fix bug in order to have seperate item and image functions

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage(newImgName, imgSrc , imgLeft , imgBottom  ){
+    var newImgName = document.createElement('img')
+    newImgName.src = imgSrc
+    newImgName.style.position = 'fixed'
+    newImgName.style.left = imgLeft
+    newImgName.style.bottom = imgBottom
+document.body.append(newImgName)
+    newImgName.addEventListener("dblclick", function(){
+    newImgName.remove()})
+}
+
+
+
+function newItem(newImgName, imgSrc , imgLeft , imgBottom){
+    newImage(newImgName, imgSrc , imgLeft , imgBottom);
+    newImgName.addEventListener("dblclick", function(){
+    newImgName.remove()})
+}
+
+newImage("tree","assets/tree.png","300px","300px")
+newImage("pillar","assets/pillar.png","350px","100px")
+newImage("crate","assets/crate.png","150px","200px")
+newImage("well","assets/well.png","500px","425px")
+newImage("greenCharacter","assets/green-character.gif","200px","200px")
+newImage("pine-tree","assets/pine-tree.png","450px","200px")
+newItem("sword","assets/sword.png","500px","405px")
+newItem("shield","assets/sheild.png","165px","185px")
+newItem("staff","assets/staff.png","600px","100px")
